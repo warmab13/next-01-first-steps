0d2f64d983e87a45dd4815b0aa2a1569f6697f45
warmab13 <alonsosalcido13@gmail.com>
2024-02-15 15:33:10 -0600

    Routes and nested routes

--- app/(general)/about/page.tsx
+++ app/(general)/about/page.tsx
@@ -9,8 +9,6 @@ export const metadata: Metadata = {
 
 export default function AboutPage(){
     return (
-        <>
-            <span className="text-7xl">About Page</span>
-        </>
+        <span className="text-7xl">About Page</span>
     )
 }
---
+++ app/(general)/layout.tsx
@@ -0,0 +1,13 @@
+
+export default function AboutLayout({
+ children
+}: {
+ children: React.ReactNode;
+}) {
+  return (
+    <main className="flex flex-col items-center p-24">
+        <span className="text-lg">Hola mundo</span>
+        {children}
+    </main>
+  );
+}
