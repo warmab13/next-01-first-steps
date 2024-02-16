dc68a5302e4297fd6aa2b6ef7e7cb88fe235ff8b
warmab13 <alonsosalcido13@gmail.com>
2024-02-16 09:21:10 -0600

    END OF SECTION 2, using a new link to change page between home and about

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center p-24">
       <span className="text-5xl"> HOLA MUNDO </span>
+
+      <Link href='/about'> About Page </Link>
     </main>
   );
 }
