e389f8a233b8538d0191460a8b3c28876aead17b
warmab13 <alonsosalcido13@gmail.com>
2024-02-19 10:00:50 -0600

    Dockerfile nextjs recommendations to create an image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'standalone', //This configuration is recommended when you are using docker containers
+};
 
 export default nextConfig;
